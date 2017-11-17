5e1b29fdb001b04108f4d7fd0e421205337369e3
Brandon Watts <brandonwatts@outlook.com>
2017-11-17 13:50:31 -0500

    Cont.

--- NERD/oneclass.py
+++ NERD/oneclass.py
@@ -1,57 +1,39 @@
-import matplotlib as mpl
-import numpy as np
-from sklearn.model_selection import train_test_split
-mpl.use('TkAgg')
-import matplotlib.pyplot as plt
 from sklearn import svm
 import arff2df
-from sklearn import metrics
-import pandas as pd
-import seaborn as sn
+import argparse
+from sklearn.externals import joblib
 
-''' Start by reading in the arff file, turning it into a dataframe, and mapping the classes to -1 and 1'''
-df = arff2df.arff2df("nanop.arff")
-df['Entity'] = df['Entity'].map({'Yes': -1, 'No': 1})
-train_set, test_set = train_test_split(df,                  # Split the data into test and training data
-                                       test_size=.2,
-                                       random_state=42)
 
-'''Create the testing labels and vectors'''
-testing_labels = test_set['Entity']
-test_set = test_set.drop(["Entity"], axis=1)
-X_test = np.array(test_set.values, np.int32)
-y_test = np.array(testing_labels.values, np.int32)
+pars = argparse.ArgumentParser(usage='Creates and evaluates a OneClassSVM in Scikit',
+                               formatter_class=argparse.RawTextHelpFormatter,
+                               description='''Creates and evaluates a OneClassSVM in Scikit''',
+                               version='0.1')
 
-'''Create the training labels and vectors'''
-training_labels = train_set['Entity']
-train_set = train_set.drop(["Entity"], axis=1)
-X_train = np.array(train_set.values, np.int32)
-y_train = np.array(training_labels.values, np.int32)
+pars.add_argument('-tr', '--train',
+                  help='Traing ARFF file')
 
-outliers = training_labels[training_labels == -1]               # Obtain the outliers
-nu = float(outliers.shape[0])/float(training_labels.shape[0])   # Compute our nu
-model = svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.00005)     # Create the One-Class Classifier
-model.fit(train_set)                                            # Train the model
+pars.add_argument('-l', '--labels',
+                  help='Name of the Labels Column')
 
-preds = model.predict(test_set)     # Predictions
-targs = testing_labels              # Target Values
+arguments = pars.parse_args()
+args = vars(arguments)
 
-''' Print Metrics '''
-print("nu", nu)
-print("accuracy: ", metrics.accuracy_score(targs, preds))
-print("precision: ", metrics.precision_score(targs, preds))
-print("recall: ", metrics.recall_score(targs, preds))
-print("f1: ", metrics.f1_score(targs, preds))
-print("area under curve (auc): ", metrics.roc_auc_score(targs, preds))
+print("Starting Mapping...")
+training_df = arff2df.arff2df(args['train'])
+training_df[args['labels']] = training_df[args['labels']].map({'Yes': -1, 'No': 1})
+print("Mapping Complete.")
 
-''' Create the confusion matrix '''
-cfMat = pd.crosstab(targs, preds, rownames=['Actual'], colnames=['Predicted'], margins=True)
-print(cfMat)
+print("Loading Training Data...")
+training_labels = training_df[args['labels']]
+training_features = training_df.drop([args['labels']], axis=1)
+print("Training Data Loaded.")
 
-''' Visualize the Confusion Matrix '''
-cf = metrics.confusion_matrix(targs,preds)
-df_cm = pd.DataFrame(cf, index = [i for i in ["Nano","Non-Nano"]], columns = [i for i in ["Nano","Non-Nano"]])
-plt.figure(figsize=(8, 5))
-sn.heatmap(df_cm,annot=True, cmap='Blues', fmt='g')
-plt.show()
+outliers = training_labels[training_labels == -1]
+nu = float(outliers.shape[0])/float(training_labels.shape[0])
+model = svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.0005)
 
+print("Begin Training Model...")
+model.fit(training_features)
+print("Model Training Complete.")
+
+joblib.dump(model, 'Models/oneclass.pkl')
