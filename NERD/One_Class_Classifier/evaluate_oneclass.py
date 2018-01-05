20a79070b8425ac1bd3083c716a7850118592340
Brandon Watts <brandonwatts@outlook.com>
2018-01-05 11:01:08 -0500

    Cleanup one class classifier

--- NERD/One_Class_Classifier/evaluate_oneclass.py
+++ NERD/One_Class_Classifier/evaluate_oneclass.py
@@ -1,4 +1,5 @@
 import matplotlib as mpl
+from Tools import arff_converter
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn import metrics
@@ -6,60 +7,26 @@ import argparse
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sn
-from Tools import arff_converter
 
-pars = argparse.ArgumentParser(usage='Creates and evaluates a OneClassSVM in Scikit',
-                               formatter_class=argparse.RawTextHelpFormatter,
-                               description='''Creates and evaluates a OneClassSVM in Scikit''')
+# Get the Command Line Arguements
+pars = argparse.ArgumentParser()
+pars.add_argument('-te', '--test', help='Testing ARFF file')
+arguments = pars.parse_args()
+args = vars(arguments)
 
-pars.add_argument('-te', '--test',
-                  help='Testing ARFF file')
+# Obtain the Dataset
+dataset = arff_converter.arff2df(args['test'])
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].map({'Yes': -1, 'No': 1}).values
 
-pars.add_argument('-l', '--labels',
-                  help='Name of the Labels Column')
+# Load the classifier and make predictions
+clf = joblib.load('../Models/oneclass.pkl')
+preds = clf.predict(X)
 
-
-def main():
-    arguments = pars.parse_args()
-    args = vars(arguments)
-    labelName = args['labels']
-    testing_df = arff_converter.arff2df(args['test'])
-    testing_labels = convert_labels_to_numeric(testing_df, labelName)
-    X_test = testing_df.drop([labelName], axis=1)
-    clf = load_classifier()
-    preds = clf.predict(X_test)
-    targs = testing_labels
-
-    cfMat = create_conf_mat(targs, preds)
-
-    print('\nConfusion Matrix\n\n{}'.format(cfMat))
-    print_metrics(targs, preds)
-    plot_conf_mat(targs, preds)
-
-
-def load_classifier():
-    return joblib.load('../Models/oneclass.pkl')
-
-
-def print_metrics(targs, preds):
-    print('\nMetrics\n')
-    print(metrics.classification_report(targs, preds))
-
-def create_conf_mat(targs, preds):
-    return pd.crosstab(targs, preds, rownames=['Actual'], colnames=['Predicted'], margins=True)
-
-
-def plot_conf_mat(targs, preds):
-    cf = metrics.confusion_matrix(targs, preds)
-    df_cm = pd.DataFrame(cf, index=[i for i in ["Nano", "Non-Nano"]], columns=[i for i in ["Nano", "Non-Nano"]])
-    plt.figure(figsize=(8, 5))
-    sn.heatmap(df_cm, annot=True, cmap='Blues', fmt='g')
-    plt.show()
-
-
-def convert_labels_to_numeric(df, labelName):
-    return df[labelName].map({'Yes': -1, 'No': 1})
-
-
-if __name__ == "__main__":
-    main()
+# Print the Metrics
+print(metrics.classification_report(y, preds))
+cf = metrics.confusion_matrix(y, preds)
+df_cm = pd.DataFrame(cf)
+plt.figure(figsize=(8, 5))
+sn.heatmap(df_cm, annot=True, cmap='Blues', fmt='g')
+plt.show()
--- NERD/One_Class_Classifier/oneclass.py
+++ NERD/One_Class_Classifier/oneclass.py
@@ -3,48 +3,22 @@ import argparse
 from sklearn.externals import joblib
 from Tools import arff_converter
 
-pars = argparse.ArgumentParser(usage='Creates and evaluates a OneClassSVM in Scikit',
-                               formatter_class=argparse.RawTextHelpFormatter,
-                               description='''Creates and evaluates a OneClassSVM in Scikit''')
+# Get the Command Line Arguements
+pars = argparse.ArgumentParser()
+pars.add_argument('-tr', '--train', help='Traing ARFF file')
+arguments = pars.parse_args()
+args = vars(arguments)
 
-pars.add_argument('-tr', '--train',
-                  help='Traing ARFF file')
+# Obtain the Dataset
+dataset = arff_converter.arff2df(args['train'])
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].map({'Yes': -1, 'No': 1}).values
 
-pars.add_argument('-l', '--labels',
-                  help='Name of the Labels Column')
+# Create and fit the classifier
+outliers = y[y == -1]
+nu = float(outliers.shape[0]) / float(y.shape[0])
+clf = svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.0005)
+clf.fit(X, y)
 
-def main():
-    arguments = pars.parse_args()
-    args = vars(arguments)
-    labelName = args['labels']
-    training_df = arff_converter.arff2df(args['train'])
-    y_train = convert_labels_to_numeric(training_df, labelName)
-    X_train = training_df.drop([args['labels']], axis=1)
-    clf = create_classifier(y_train)
-    clf.fit(X_train, y_train)
-    joblib.dump(clf, '../Models/oneclass.pkl')
-
-
-def convert_labels_to_numeric(df, labelName):
-    '''
-    Method to Convert the Yes and No Columns to -1 and 1 numerical values
-    :param df: Dataframe
-    :param labelName: Name of the Entity Label
-    :return: DataFrame with updated Labels
-    '''
-    return df[labelName].map({'Yes': -1, 'No': 1})
-
-
-def create_classifier(training_labels):
-    '''
-    Method to create the one class classifier
-    :param training_labels: all of the labels for training
-    :return: One_Class_Classifier
-    '''
-    outliers = training_labels[training_labels == -1]
-    nu = float(outliers.shape[0]) / float(training_labels.shape[0])
-    return svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.0005)
-
-
-if __name__ == "__main__":
-    main()
+# Save the classifier
+joblib.dump(clf, '../Models/oneclass.pkl')
