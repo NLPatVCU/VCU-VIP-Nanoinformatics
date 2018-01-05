cf7dd67312bf3e117e501b67beaef5e51f872780
Brandon Watts <brandonwatts@outlook.com>
2018-01-05 11:01:32 -0500

    Add CRF Stuff

---
+++ NERD/CRF/CRF.py
@@ -0,0 +1,126 @@
+from itertools import chain
+import nltk
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelBinarizer
+import pycrfsuite
+from collections import Counter
+
+
+def main():
+
+    """
+    First we collect our test and train sets. In the following format:
+        [
+            [
+                (Token, POS TAG, ENTITY)
+                .
+                .
+            ],
+            [
+            .
+            .
+            ]
+        ]
+    """
+    train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
+    test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+
+    '''Next We seperate our features and labels for both test and train sets'''
+    X_train = [sent2features(s) for s in train_sents]
+    y_train = [sent2labels(s) for s in train_sents]
+    X_test = [sent2features(s) for s in test_sents]
+    y_test = [sent2labels(s) for s in test_sents]
+
+    '''Create and train the classifier'''
+    trainer = pycrfsuite.Trainer(verbose=False)
+    for xseq, yseq in zip(X_train, y_train):
+        trainer.append(xseq, yseq)
+    trainer.train('conll2002-esp.crfsuite')
+
+    '''Make Preditions from the trained tagger'''
+    tagger = pycrfsuite.Tagger()
+    tagger.open('conll2002-esp.crfsuite')
+    y_pred = [tagger.tag(xseq) for xseq in X_test]
+
+    '''Print the Metrics'''
+    print(bio_classification_report(y_test, y_pred))
+
+
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'word.isupper=%s' % word.isupper(),
+        'word.istitle=%s' % word.istitle(),
+        'word.isdigit=%s' % word.isdigit(),
+        'postag=' + postag,
+        'postag[:2]=' + postag[:2],
+    ]
+    if i > 0:
+        word1 = sent[i - 1][0]
+        postag1 = sent[i - 1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:postag=' + postag1,
+            '-1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('BOS')
+
+    if i < len(sent) - 1:
+        word1 = sent[i + 1][0]
+        postag1 = sent[i + 1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:postag=' + postag1,
+            '+1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('EOS')
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+
+def bio_classification_report(y_true, y_pred):
+    """
+    Classification report for a list of BIO-encoded sequences.
+    It computes token-level metrics and discards "O" labels.
+    """
+    lb = LabelBinarizer()
+    y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+
+    tagset = set(lb.classes_) - {'O'}
+    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+
+    return classification_report(
+        y_true_combined,
+        y_pred_combined,
+        labels=[class_indices[cls] for cls in tagset],
+        target_names=tagset,
+    )
+
+
+if __name__ == "__main__":
+    main()
