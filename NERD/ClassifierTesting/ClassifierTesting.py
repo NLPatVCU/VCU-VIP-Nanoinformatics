1669ce4bf5c7aec1c3def9f3dfa4970e275b9f27
Andriy Mulyar <andriy.mulyar@gmail.com>
2018-01-11 17:19:51 -0500

    small changes done after transition to willow.

--- NERD/ClassifierTesting/ClassifierTesting.py
+++ NERD/ClassifierTesting/ClassifierTesting.py
@@ -1,45 +1,19 @@
 from sklearn import svm
-from collections import Counter
+import argparse
 from sklearn.externals import joblib
-from scipy.io import arff
-from pandas import DataFrame
-
-from imblearn.over_sampling import SMOTE, ADASYN
+from Tools import arff_converter
 
 
-data, meta = arff.loadarff("../ARFF_Files/nanoparticle_ARFF/_om/_train/nonsparse_nanoparticle_train-1.arff")
-training_df = DataFrame(data=data, columns=meta.names())
+# Obtain the Dataset
+dataset = arff_converter.arff2df("../ARFF_Files/nanoparticle_ARFF/_o/_train/nanoparticle_train-1.arff")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].map({'Yes': -1, 'No': 1}).values
 
+# Create and fit the classifier
+outliers = y[y == -1]
+nu = float(outliers.shape[0]) / float(y.shape[0])
+clf = svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.0005)
+clf.fit(X, y)
 
-training_labels = training_df["Entity"].map({'Yes': -1, 'No': 1})
-training_features = training_df.drop(["Entity"], axis=1);
-
-X_resampled, y_resampled = SMOTE(ratio={-1:500}).fit_sample(training_features.values, training_labels.values)
-
-
-
-print(sorted(Counter(y_resampled).items()))
-
-outliers = y_resampled[y_resampled == -1]
-nu = float(outliers.shape[0]) / float(y_resampled.shape[0])
-
-exit()
-clf = svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.0005);
-clf.fit(X_resampled, y=y_resampled)
+# Save the classifier
 joblib.dump(clf, '../Models/testmodel_oneclass.pkl')
-
-
-
-
-
-def create_classifier(training_labels):
-    '''
-    Method to create the one class classifier
-    :param training_labels: all of the labels for training
-    :return: One_Class_Classifier
-    '''
-    outliers = training_labels[training_labels == -1]
-    nu = float(outliers.shape[0]) / float(training_labels.shape[0])
-    return svm.OneClassSVM(nu=nu, kernel='rbf', gamma=0.0005)
-
-
--- NERD/ClassifierTesting/EvaluateClassifier.py
+++ NERD/ClassifierTesting/EvaluateClassifier.py
@@ -1,50 +1,31 @@
-#import matplotlib as mpl
-from scipy.io import arff
-from pandas import DataFrame
+import matplotlib as mpl
+from Tools import arff_converter
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn import metrics
+import argparse
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sn
 
 
-labelName = 'Entity'
-data, meta = arff.loadarff("../ARFF_Files/nanoparticle_ARFF/_om/_test/nonsparse_nanoparticle_test-1.arff")
-testing_df = DataFrame(data=data, columns=meta.names())
-testing_labels = testing_df[labelName].map({'Yes': 1, 'No': -1})
-testing_features = testing_df.drop([labelName], axis=1)
+# Obtain the Dataset
+dataset = arff_converter.arff2df("../ARFF_Files/nanoparticle_ARFF/_o/_test/nanoparticle_test-1.arff")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].map({'Yes': 1, 'No': -1}).values
 
-clf = joblib.load('../Models/testmodel_oneclass_om_1.pkl')
-preds = clf.predict(testing_features)
-for i, val in enumerate(preds):
-    if val==1:
-        preds[i] = -1;
-    else:
-        preds[i] = 1;
+# Load the classifier and make predictions
+clf = joblib.load('../Models/test_classifier_newdataset.pkl')
+preds = clf.predict(X)
 
-targs = testing_labels
+print(preds)
+for i, pred in enumerate(preds):
+    preds[i] *= -1;
 
-print metrics.confusion_matrix(targs,preds)
-print metrics.precision_score(targs,preds)
-print metrics.recall_score(targs,preds)
-
-
-def create_conf_mat(targs, preds):
-    return pd.crosstab(targs, preds, rownames=['Actual'], colnames=['Predicted'], margins=True)
-
-
-def plot_conf_mat(targs, preds):
-    cf = metrics.confusion_matrix(targs, preds)
-    df_cm = pd.DataFrame(cf, index=[i for i in ["Nano", "Non-Nano"]], columns=[i for i in ["Nano", "Non-Nano"]])
-    plt.figure(figsize=(8, 5))
-    sn.heatmap(df_cm, annot=True, cmap='Blues', fmt='g')
-    plt.show()
-
-def print_metrics(targs, preds):
-    print('\nMetrics\n')
-    print("accuracy: ", metrics.accuracy_score(targs, preds))
-    print("precision: ", metrics.precision_score(targs, preds))
-    print("recall: ", metrics.recall_score(targs, preds))
-    print("f1: ", metrics.f1_score(targs, preds))
-    print("Geometric Mean", metrics.fowlkes_mallows_score(targs, preds))
-    print("area under curve (auc): ", metrics.roc_auc_score(targs, preds))
+# Print the Metrics
+print(metrics.precision_score(y,preds))
+cf = metrics.confusion_matrix(y, preds)
+df_cm = pd.DataFrame(cf)
+plt.figure(figsize=(8, 5))
+sn.heatmap(df_cm, annot=True, cmap='Blues', fmt='g')
+plt.show()
