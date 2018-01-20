885ad750fd6abfcf6ec13abee515b6c821191c6b
Andriy Mulyar <andriy.mulyar@gmail.com>
2018-01-20 15:36:48 -0500

    Began working with decision trees for NER classifier

--- NERD/ClassifierTesting/EvaluateClassifier.py
+++ NERD/ClassifierTesting/EvaluateClassifier.py
@@ -11,18 +11,15 @@ import seaborn as sn
 # Obtain the Dataset
 dataset = arff_converter.arff2df("../ARFF_Files/nanoparticle_ARFF/_o/_test/nanoparticle_test-1.arff")
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].map({'Yes': 1, 'No': -1}).values
+y = dataset.iloc[:, -1].map({'Yes': -1, 'No': 1}).values
 
 # Load the classifier and make predictions
-clf = joblib.load('../Models/test_classifier_newdataset.pkl')
+clf = joblib.load('../Models/oneclasssvm_omt_1.pkl')
 preds = clf.predict(X)
 
-print(preds)
-for i, pred in enumerate(preds):
-    preds[i] *= -1;
 
 # Print the Metrics
-print(metrics.precision_score(y,preds))
+print(metrics.precision_score(y,preds, average=None)[0])
 cf = metrics.confusion_matrix(y, preds)
 df_cm = pd.DataFrame(cf)
 plt.figure(figsize=(8, 5))
