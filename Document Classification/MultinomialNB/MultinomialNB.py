62b0975d88601bbd589dd3b79d08d2038931e56b
Brandon Watts <brandonwatts@outlook.com>
2018-01-29 14:41:47 -0500

    Doc classification updates

--- Document Classification/MultinomialNB/MultinomialNB.py
+++ Document Classification/MultinomialNB/MultinomialNB.py
@@ -1,35 +1,60 @@
-import pandas as pd
 import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import KFold
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 import Tools.TextTools as TextTools
-from sklearn import metrics
-import sys
+import pandas as pd
 
-documents_map = []
-documents_mapping_df = pd.read_csv(sys.argv[1])
 
-for index, row in documents_mapping_df.iterrows():
-    documents_map.append(TextTools.build_data_frame(row["Label"], row['Directory']))
+def shuffle(documents):
+    return documents.reindex(np.random.permutation(documents.index))
 
-documets = pd.concat(documents_map)
-documets['text'] = documets['text'].map(lambda x: TextTools.clean_text(x)).map(lambda x: TextTools.detokenize(x))
-documets = documets.reindex(np.random.permutation(documets.index))
 
-X = documets.iloc[:, 1].values
-y = documets.iloc[:, 0].values
+# Load the Data
+mapping_df = pd.read_csv('mapping.csv')
+documents_array = []
+for index, row in mapping_df.iterrows():
+    documents_array.append(TextTools.build_data_frame(row["Directory"], row['Label']))
+documents = pd.concat(documents_array)
+documents = shuffle(documents)
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state = 0)
 
-count_vectorizer = CountVectorizer()
-counts = count_vectorizer.fit_transform(X_train)
-classifier = MultinomialNB()
-targets = y_train
-classifier.fit(counts, targets)
+# Clean the Data
+documents['text'] = documents['text'].map(lambda x: TextTools.clean_text(x))
 
-preds = classifier.predict(count_vectorizer.transform(X_test))
+X = documents.iloc[:, 1].values
+y = documents.iloc[:, 0].values
 
-print(metrics.classification_report(y_test, preds))
+# Create Data Pipeline
+pipeline = Pipeline([
+    ('vectorizer', CountVectorizer(ngram_range=(1, 2))),
+    ('classifier', MultinomialNB())
+])
 
+# Make Predictions
+k_fold = KFold(n_splits=3)
+confusion = np.zeros(shape=(4,4))
+accuracy = 0
+for train_index, test_index in k_fold.split(X):
+
+    train_text, test_text = X[train_index],  X[test_index]
+    train_y, test_y = y[train_index], y[test_index]
+
+    pipeline.fit(train_text, train_y)
+    predictions = pipeline.predict(test_text)
+
+    y_actu = pd.Series(test_y, name='Actual')
+    y_pred = pd.Series(predictions, name='Predicted')
+    df_confusion = pd.crosstab(y_actu, y_pred)
+    print(df_confusion)
+
+    confusion += confusion_matrix(test_y, predictions)
+    accuracy += accuracy_score(test_y, predictions)
+    print(classification_report(test_y, predictions))
+    print("Accuracy: ", accuracy_score(test_y, predictions))
+
+print('Confusion matrix:')
+print(confusion)
+print("Accuracy: ", accuracy/3)
