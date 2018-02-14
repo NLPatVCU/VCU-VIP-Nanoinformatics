f7aef354124e0c7a98994b331ea6f799bc626357
Brandon Watts <brandonwatts@outlook.com>
2018-02-14 12:31:17 -0500

    Fix LSTM

--- Document Classification/Binary/CNN/CNN.py
+++ Document Classification/Binary/CNN/CNN.py
@@ -1,80 +1,85 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import pandas as pd
+import re
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM
 from sklearn.model_selection import train_test_split
-from keras.utils.np_utils import to_categorical
-import re
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
-abstracts = pd.read_json("../Data/DOIData.json")
-abstracts['is_nano'] = 1
-abstracts = abstracts.reindex(np.random.permutation(abstracts.index))
-with pd.option_context('expand_frame_repr', False):
-    print(abstracts)
+def main():
+    """ Preprocessing Steps """
 
+    PMC_NONNANO_abstracts = pd.read_json("../Data/JSON/Non-Nano.json")
+    PMC_NONNANO_abstracts = PMC_NONNANO_abstracts.sample(500)
+    PMC_NONNANO_abstracts['is_nano'] = "No"
+    PMC_abstracts = pd.read_json("../Data/JSON/Nano.json")
+    PMC_abstracts['is_nano'] = 'Yes'
+    DOI_abstracts = pd.read_json("../Data/JSON/DOIData.json")
+    DOI_abstracts['is_nano'] = 'Yes'
+    DOI_abstracts['abstract'] = DOI_abstracts['abstract'].apply(lambda x: re.sub("\n", "", x))  # Remove new lines
+    abstracts = pd.concat([PMC_abstracts, DOI_abstracts, PMC_NONNANO_abstracts], ignore_index=True)  # Combine data
+    abstracts = abstracts[abstracts.abstract != "No Abstract Provided"]  # Remove papers with no abstract
+    abstracts = abstracts[abstracts.abstract.str.strip() != ""]  # Remove papers with empty abstracts
+    abstracts = abstracts.reindex(np.random.permutation(abstracts.index))
 
-print("Number of Nanofiles:", abstracts[abstracts['is_nano'] == 1].shape[0])
-print("Number of Non-Nanofiles:", abstracts[abstracts['is_nano'] == 0].shape[0])
+    with pd.option_context('expand_frame_repr', False):
+        print(abstracts)
 
-
-max_fatures = 2000
-tokenizer = Tokenizer(num_words=max_fatures, split=' ')
-tokenizer.fit_on_texts(abstracts['abstract'].values)
-X = tokenizer.texts_to_sequences(abstracts['abstract'].values)
-X = pad_sequences(X)
-
-embed_dim = 128
-lstm_out = 196
-
-model = Sequential()
-model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
-model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(2,activation='softmax'))
-model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
-print(model.summary())
-
-y = abstracts["is_nano"].values
-
-X_train, X_test, Y_train, Y_test = train_test_split(X,y, test_size = 0.33, random_state = 42)
-print(X_train.shape,Y_train.shape)
-print(X_test.shape,Y_test.shape)
+    print("Number of Nanofiles:", abstracts[abstracts['is_nano'] == "Yes"].shape[0])
+    print("Number of Non-Nanofiles:", abstracts[abstracts['is_nano'] == "No"].shape[0])
 
 
-"""
+    """ Model Training """
 
-NUMBER_OF_FOLDS = 10
-X = documents.iloc[:, 1].values
-y = documents.iloc[:, 0].values
+    max_features = 2000
+    tokenizer = Tokenizer(num_words=max_features, split=' ')
+    tokenizer.fit_on_texts(abstracts['abstract'].values)
+    X = tokenizer.texts_to_sequences(abstracts['abstract'].values)
+    X = pad_sequences(X)
 
-pipeline = Pipeline([
-    ('vectorizer', CountVectorizer(ngram_range=(1, 2))),
-    ('classifier', MultinomialNB())
-])
+    embed_dim = 128
+    lstm_out = 196
 
-scoreers = {
-        "f1_scores": make_scorer(f1_score, average='weighted'),
-        "precision_scores": make_scorer(precision_score, average='weighted'),
-        "recall_scores": make_scorer(recall_score, average='weighted'),
-    }
-scores = cross_validate(pipeline, X, y, cv=NUMBER_OF_FOLDS,scoring=scoreers, n_jobs=-1, return_train_score=False)
+    model = Sequential()
+    model.add(Embedding(max_features, embed_dim, input_length=X.shape[1]))
+    model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
 
-f1_scores = scores['test_f1_scores']
-precision_scores = scores['test_precision_scores']
-recall_scores = scores['test_recall_scores']
+    y = pd.get_dummies(abstracts['is_nano']).values
 
-for x in range(NUMBER_OF_FOLDS):
-    print("Fold number: ", x)
-    print("Precision: ", precision_scores[x])
-    print("Recall: ", recall_scores[x])
-    print("F1 Score: ", f1_scores[x])
-    print("\n")
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    print(X_train.shape,Y_train.shape)
+    print(X_test.shape,Y_test.shape)
 
-print("Averages Across Folds")
-print("Precision: ", np.mean(np.array(precision_scores)))
-print("Recall: ", np.mean(np.array(recall_scores)))
-print("F1 Score: ", np.mean(np.array(f1_scores)))
-"""
+    model.fit(X_train, Y_train, epochs=10, batch_size=32, verbose=2)
+
+    y_pred = model.predict(X_test, verbose=0, batch_size=32)
+    y_pred = np.around(y_pred).astype('int')
+    y_pred = [np.argmax(x) for x in y_pred]
+    Y_test = [np.argmax(x) for x in Y_test]
+
+    print("Y pred:", y_pred)
+    print("Y True", Y_test)
+
+    acc = accuracy_score(y_pred=y_pred, y_true=Y_test)
+    precision = precision_score(y_pred=y_pred, y_true=Y_test, average='micro')
+    recall = recall_score(y_pred=y_pred, y_true=Y_test)
+    f1 = f1_score(y_pred=y_pred, y_true=Y_test)
+    confmat = confusion_matrix(y_pred=y_pred, y_true=Y_test)
+
+
+
+    print("Accuracy:", acc)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1 score:", f1)
+    print("Confusion Matrix")
+    print(confmat)
+
+
+if __name__ == "__main__":
+    main()
